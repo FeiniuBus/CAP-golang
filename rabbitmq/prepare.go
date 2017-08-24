f5c9e9e5c2b74e122964be98d459ce20592fb909
hongyanshen <shy@feiniubus.com>
2017-08-24 17:01:30 +0800

    ++ statechanger

--- rabbitmq/prepare.go
+++ rabbitmq/prepare.go
@@ -12,4 +12,7 @@ func Prepare(bootstrapper *cap.Bootstrapper, rabbitMQOptions RabbitMQOptions ) {
 		bootstrapper.ConnectionFactory,
 	)
 	bootstrapper.Servers = append(bootstrapper.Servers, consumerHandler)
+	bootstrapper.QueueExecutorFactory.SetPublishQueueExecutorCreateDelegate(func () cap.IQueueExecutor{
+		return NewPublishQueueExecutor(nil, &rabbitMQOptions)
+	})
 }
