301fbda593199c634d14f9301c40b2181bfb47ff
andrew.cui <mykge@outlook.com>
2017-08-24 11:24:19 +0800

    no message

--- captesting/main.go
+++ captesting/main.go
@@ -7,23 +7,34 @@ import (
 	_ "github.com/go-sql-driver/mysql" 
 )
 
-var CapOptions cap.CapOptions
+var CapOptions *cap.CapOptions
 var ConnectionString string
 var PublisherFactory *cap.PublisherFactory
+var StorageConnectionFactory *cap.StorageConnectionFactory
+var Dispatcher *cap.Dispatcher
 
-func CreatePublisher(options cap.CapOptions) (cap.IPublisher, error) {
+func CreatePublisher(options *cap.CapOptions) (cap.IPublisher, error) {
 	result := cmysql.NewPublisher(options)
 	return result, nil
 }
 
+func CreateStorageConnection(options *cap.CapOptions)(cap.IStorageConnection, error){
+	result := cmysql.NewStorageConnection(options)
+	return result, nil
+}
+
 func init(){
-	CapOptions = cap.CapOptions{}
+	CapOptions = &cap.CapOptions{}
 	ConnectionString = "root:kge2001@tcp(192.168.206.129:3306)/FeiniuCAP?charset=utf8"
 	CapOptions.UseMySql(ConnectionString)
 	PublisherFactory = cap.NewPublisherFactory(CreatePublisher) 
+	StorageConnectionFactory = cap.NewStorageConnectionFactory(CreateStorageConnection)
+	Dispatcher = cap.NewDispatcher(CapOptions,StorageConnectionFactory)
 }
 
 func main(){
+	Dispatcher.Begin()
+	
 	publisher, err := PublisherFactory.CreatePublisher(CapOptions)
 	if err != nil {
 		panic(err)
@@ -41,4 +52,12 @@ func main(){
 	if err != nil {
 		panic(err)
 	}
+	err = trans.Commit()
+	if err != nil {
+		panic(err)
+	}
+
+	
+
+	select{}
 }
