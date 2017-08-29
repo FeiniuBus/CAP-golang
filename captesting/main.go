9bfe6b09b4e9bfd192aa8cedd9c9ed78cbf220b2
andrew.cui <mykge@outlook.com>
2017-08-29 11:50:15 +0800

    no message

--- captesting/main.go
+++ captesting/main.go
@@ -2,62 +2,85 @@ package main
 
 import (
 	"database/sql"
+
 	"github.com/FeiniuBus/capgo"
 	cmysql "github.com/FeiniuBus/capgo/mysql"
-	_ "github.com/go-sql-driver/mysql" 
+	crabbitmq "github.com/FeiniuBus/capgo/rabbitmq"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 var CapOptions *cap.CapOptions
+var RabbitMQOptions *crabbitmq.RabbitMQOptions
 var ConnectionString string
 var PublisherFactory *cap.PublisherFactory
 var StorageConnectionFactory *cap.StorageConnectionFactory
-var Dispatcher *cap.Dispatcher
+var ProcessorServer *cap.ProcessorServer
+var CallbackRegister *cap.CallbackRegister
+var Bootstrapper *cap.Bootstrapper
 
 func CreatePublisher(options *cap.CapOptions) (cap.IPublisher, error) {
 	result := cmysql.NewPublisher(options)
 	return result, nil
 }
 
-func CreateStorageConnection(options *cap.CapOptions)(cap.IStorageConnection, error){
+func CreateStorageConnection(options *cap.CapOptions) (cap.IStorageConnection, error) {
 	result := cmysql.NewStorageConnection(options)
 	return result, nil
 }
 
-func init(){
+func init() {
 	CapOptions = cap.NewCapOptions()
 	ConnectionString = "root:kge2001@tcp(192.168.206.129:3306)/FeiniuCAP?charset=utf8"
-	CapOptions.UseMySql(ConnectionString)
-	PublisherFactory = cap.NewPublisherFactory(CreatePublisher) 
-	StorageConnectionFactory = cap.NewStorageConnectionFactory(CreateStorageConnection)
-	Dispatcher = cap.NewDispatcher(CapOptions,StorageConnectionFactory)
-}
+	CapOptions.ConnectionString = ConnectionString
+	ProcessorServer = cap.NewProcessorServer()
+	ProcessorServer.Container.Register(cap.NewFailedJobProcessor(CapOptions, StorageConnectionFactory))
+	ProcessorServer.Container.Register(cap.NewPublishQueuer(CapOptions, StorageConnectionFactory))
+	ProcessorServer.Container.Register(cap.NewSubscribeQueuer(CapOptions, StorageConnectionFactory))
+	ProcessorServer.Container.Register(cap.NewDefaultDispatcher(CapOptions, StorageConnectionFactory))
 
-func main(){
-	
+	Bootstrapper = cap.NewBootstrapper(CapOptions, StorageConnectionFactory)
+	Bootstrapper.Servers = append(Bootstrapper.Servers,ProcessorServer)
+
+	RabbitMQOptions = crabbitmq.RabbitMQConfig
+	RabbitMQOptions.SetHostName("192.168.206.128")
+	RabbitMQOptions.SetUserName("andrew")
+	RabbitMQOptions.SetPassword("kge2001")
+	crabbitmq.Prepare(Bootstrapper, *RabbitMQOptions)
+
+	PublisherFactory = cap.NewPublisherFactory(CreatePublisher)
+	StorageConnectionFactory = cap.NewStorageConnectionFactory(CreateStorageConnection)
+
 	publisher, err := PublisherFactory.CreatePublisher(CapOptions)
 	if err != nil {
 		panic(err)
 	}
-	conn, err := sql.Open("mysql", ConnectionString)
-	defer conn.Close()
-	if err != nil {
-		panic(err)
-	}
-	trans, err := conn.Begin()
-	if err != nil {
-		panic(err)
-	}
-	err = publisher.Publish("test", "test", conn, trans)
-	if err != nil {
-		panic(err)
-	}
-	err = trans.Commit()
+
+	dbConnection, err := sql.Open("mysql", ConnectionString)
+	defer dbConnection.Close()
 	if err != nil {
 		panic(err)
 	}
 
-	
-	go Dispatcher.Begin()
+	dbTransaction, err := dbConnection.Begin()
+	if err != nil {
+		panic(err)
+	}
 
-	select{}
+	err = publisher.Publish("test", "test", dbConnection, dbTransaction)
+	if err != nil {
+		panic(err)
+	}
+	err = dbTransaction.Commit()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	go Bootstrapper.Bootstrap()
+
+	for {
+
+	}
+	//Bootstrapper.WaitForTerminalSignal()
 }
