44b07f8ad56a0f85142d0b4252a36ab76d2571ff
andrew.cui <mykge@outlook.com>
2017-08-29 14:58:14 +0800

    fix stateChanger

--- istate_changer.default.go
+++ istate_changer.default.go
@@ -1,20 +1,24 @@
 package cap
 
-type StateChanger struct{
-	
+// StateChanger ..
+type StateChanger struct {
 }
 
-func NewStateChanger() IStateChanger{
+// NewStateChanger ..
+func NewStateChanger() IStateChanger {
 	stateChanger := &StateChanger{}
 	return stateChanger
 }
 
-func (this *StateChanger) ChangeReceivedMessageState(message *CapReceivedMessage, state IState, transaction IStorageTransaction) error{
+// ChangeReceivedMessageState ...
+func (stateChanger *StateChanger) ChangeReceivedMessageState(message *CapReceivedMessage, state IState, transaction IStorageTransaction) error {
 	message.StatusName = state.GetName()
 	err := state.ApplyReceivedMessage(message, transaction)
 	if err != nil {
 		return err
 	}
+	message.StatusName = state.GetName()
+	message.ExpiresAt = int(state.GetExpiresAfter())
 	err = transaction.UpdateReceivedMessage(message)
 	if err != nil {
 		return err
@@ -22,12 +26,15 @@ func (this *StateChanger) ChangeReceivedMessageState(message *CapReceivedMessage
 	return nil
 }
 
-func (this *StateChanger) ChangePublishedMessage(message *CapPublishedMessage, state IState, transaction IStorageTransaction) error{
+// ChangePublishedMessage ...
+func (stateChanger *StateChanger) ChangePublishedMessage(message *CapPublishedMessage, state IState, transaction IStorageTransaction) error {
 	message.StatusName = state.GetName()
 	err := state.ApplyPublishedMessage(message, transaction)
 	if err != nil {
 		return err
 	}
+	message.StatusName = state.GetName()
+	message.ExpiresAt = int(state.GetExpiresAfter())
 	err = transaction.UpdatePublishedMessage(message)
 	if err != nil {
 		return err
