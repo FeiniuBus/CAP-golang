780f422095dbbc3a84316a42132269274a507ee5
andrew.cui <mykge@outlook.com>
2017-08-30 15:38:40 +0800

    no message

--- ipublisher.go
+++ ipublisher.go
@@ -1,6 +1,8 @@
 package cap
 
+// IPublisher ...
 type IPublisher interface {
 	Publish(descriptors []*MessageDescriptor, connection interface{}, transaction interface{}) error
-	PublishOne(name string, content interface, connection interface{}, transaction interface{}) error
+
+	PublishOne(name string, content interface{}, connection interface{}, transaction interface{}) error
 }
