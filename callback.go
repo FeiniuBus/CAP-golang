3903f64911d516be44cda8364c28c9a88022d2c7
hongyanshen <shy@feiniubus.com>
2017-08-30 14:45:30 +0800

    delete default callback

--- callback.go
+++ callback.go
@@ -1,17 +1,9 @@
 package cap
 
-import (
-	"errors"
-)
-
 // Callback provide Sample Handler.
 type Callback struct {
 }
 
 type CallbackInterface interface {
 	Handle(msg interface{}) error
-}
-
-func (this *Callback) Handle(msg interface{}) error {
-	return errors.New("Custome callback must impl CallbackInterface")
 }
