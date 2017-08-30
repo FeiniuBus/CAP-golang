4dda65d01f619b6c75d79e44e35321981d9df4c6
andrew.cui <mykge@outlook.com>
2017-08-30 10:52:24 +0800

    use Ticker

--- mysql/mysqlfetchedmessage.go
+++ mysql/mysqlfetchedmessage.go
@@ -2,70 +2,75 @@ package mysql
 
 import (
 	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
 	"sync"
 	"time"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-type MySqlFetchedMessage struct{
-	messageId int
-	messageType int
-	dbConnection *sql.DB
+// MySqlFetchedMessage ...
+type MySqlFetchedMessage struct {
+	messageId     int
+	messageType   int
+	dbConnection  *sql.DB
 	dbTransaction *sql.Tx
-	mutext *sync.Mutex
+	mutext        *sync.Mutex
+	ticker        *time.Ticker
 }
 
-func NewFetchedMessage(_messageId int, _messageType int, _dbConnection *sql.DB, _dbTransaction *sql.Tx)(*MySqlFetchedMessage){
+// NewFetchedMessage ...
+func NewFetchedMessage(_messageId int, _messageType int, _dbConnection *sql.DB, _dbTransaction *sql.Tx) *MySqlFetchedMessage {
 	result := &MySqlFetchedMessage{}
 	result.messageId = _messageId
 	result.messageType = _messageType
 	result.dbConnection = _dbConnection
 	result.dbTransaction = _dbTransaction
 	result.mutext = &sync.Mutex{}
-	go result.keeyAlive()
+	result.ticker = time.NewTicker(1 * time.Minute)
+	go result.keepAlive()
 	return result
 }
 
-func (fetchedMessage *MySqlFetchedMessage) GetMessageId()(messageId int){
+// GetMessageId ...
+func (fetchedMessage *MySqlFetchedMessage) GetMessageId() (messageId int) {
 	return fetchedMessage.messageId
 }
 
-func (fetchedMessage *MySqlFetchedMessage) GetMessageType()(messageType int){
+// GetMessageType ...
+func (fetchedMessage *MySqlFetchedMessage) GetMessageType() (messageType int) {
 	return fetchedMessage.messageType
 }
 
-func (fetchedMessage *MySqlFetchedMessage) RemoveFromQueue() error{
+// RemoveFromQueue ...
+func (fetchedMessage *MySqlFetchedMessage) RemoveFromQueue() error {
 	fetchedMessage.mutext.Lock()
 	err := fetchedMessage.dbTransaction.Commit()
 	fetchedMessage.mutext.Unlock()
 	return err
 }
 
-func (fetchedMessage *MySqlFetchedMessage) Requeue() error{
+// Requeue ...
+func (fetchedMessage *MySqlFetchedMessage) Requeue() error {
 	fetchedMessage.mutext.Lock()
 	err := fetchedMessage.dbTransaction.Rollback()
 	fetchedMessage.mutext.Unlock()
 	return err
 }
 
-func (fetchedMessage *MySqlFetchedMessage) Dispose() error{
+// Dispose ...
+func (fetchedMessage *MySqlFetchedMessage) Dispose() error {
 	fetchedMessage.mutext.Lock()
+	fetchedMessage.ticker.Stop()
 	err := fetchedMessage.dbConnection.Close()
 	fetchedMessage.mutext.Unlock()
 	return err
 }
 
-func (fetchedMessage *MySqlFetchedMessage) keeyAlive(){
-	statement := "SELECT 1"
-	tick := time.Tick(60 * time.Second)
-	for{
-		select{
-		case <- tick :
-			fetchedMessage.mutext.Lock()
-			_, _ = fetchedMessage.dbTransaction.Exec(statement)
-			fetchedMessage.mutext.Unlock()
-		default:
-			
-		}
+func (fetchedMessage *MySqlFetchedMessage) keepAlive() {
+	statement := "SELECT 1;"
+	for _ = range fetchedMessage.ticker.C {
+		fetchedMessage.mutext.Lock()
+		_, _ = fetchedMessage.dbTransaction.Exec(statement)
+		fetchedMessage.mutext.Unlock()
 	}
 }
